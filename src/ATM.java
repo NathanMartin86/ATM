4bfd7f5a059cef341e9632eafe5c64d9de977954
macbookair <nj.martin123@gmail.com>
2015-10-09 13:37:55 -0400

    ATM homework 1.4

--- src/ATM.java
+++ src/ATM.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -5,35 +6,54 @@ import java.util.Scanner;
  */
 public class ATM {
     public static void run() throws Exception {
-        System.out.println("What is your name?");
-
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        if (name.equals("")) {
-            throw new Exception("ERROR");
-        }
-        System.out.println("Welcome," + name);
-
-
-        System.out.println("Would you like to..");
-        System.out.println("[1] Check your balance");
-        System.out.println("[2] Withdraw Funds");
-        System.out.println("[3] Cancel");
-        String option = scanner.nextLine();
-        if (option.equals("1")) {
-            System.out.println("Your Balance is $1000");
-        } else if (option.equals("2")) {
-            System.out.println("How much do you want to withdraw?");
-            String amount = scanner.nextLine();
-            int amountNum = Integer.valueOf(amount);
-            if (amountNum < 100) {
-                System.out.println("Thank You, Have a Nice Day");
-            } else if (amountNum > 100) {
-                System.out.println("You have exceeded your withdrawal limit");
+        HashMap<String, Double> accounts = new HashMap();//note the Syntax for creating the HashMap
+        while (true) {
+            System.out.println("What is your name?");
+            Scanner scanner = new Scanner(System.in);
+            String name = scanner.nextLine();
+            if (name.equals("")) {
+                throw new Exception("ERROR");
             }
 
-        }if (option.equals("3")){
-            System.out.println("Thanks you, Have a nice Day");
+
+            if (!accounts.containsKey(name)) {
+                System.out.println("No Account Recognized, But Don't Worry We've Created an Account for You");
+                accounts.put(name, 100.0);//note the code for adding to the HashMap. Also note that 'name' is the object key.
+
+            }
+            else if (accounts.containsKey(name)){
+                System.out.println("Welcome Back" + ""+ name+"!");
+            }
+
+
+                System.out.println("Would you like to..");
+                System.out.println("[1] Check your balance");
+                System.out.println("[2] Withdraw Funds");
+                System.out.println("[3] Cancel");
+                String option = scanner.nextLine();
+
+                double balance = accounts.get(name);// This actually retrieves the balance.In this case 'name' is the object key. So even if you wanted the double from the Hashmap, you stil input the name.
+
+                if (option.equals("1")) {
+                    System.out.println("Your Balance is " + balance);
+                } else if (option.equals("2")) {
+                    System.out.println("How much do you want to withdraw?");
+                    String amount = scanner.nextLine();
+                    int amountNum = Integer.valueOf(amount);//Creation of AmountNum int
+                    if (amountNum < balance) {
+                        System.out.println("Thank You, Have a Nice Day");
+                        accounts.put(name, balance - amountNum);
+                    } else if (amountNum > balance) {
+                        System.out.println("You have exceeded your withdrawal limit");
+                    }
+
+                }
+                if (option.equals("3")) {
+                    System.out.println("Thank you, Have a nice Day");
+                    System.exit(0);
+                }
+
+            }
         }
+
     }
-}
